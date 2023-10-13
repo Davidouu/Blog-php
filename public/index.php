2b7dcd0ca2be5d74571bd3b23c7ebd235bfa93ec
david.didnik <david.didnik@leplusduweb.com>
2023-10-13 15:07:12 +0200

    [upgrade] : upgrade the router with using the routes array

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+use App\Config\Routes;
 use App\Router\Router;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
@@ -11,6 +12,10 @@ $twig = new Environment($loader, ['debug' => true]);
 
 $router = new Router($_GET['url'], $twig);
 
-$router->get('/', 'HomeController#index');
+foreach (Routes::getRoutes() as $name => $route) {
+    foreach ($route as $method => $params) {
+        $router->$method($params['path'], $params['controller'].'#'.$params['method']);
+    }
+}
 
 echo $router->run();
